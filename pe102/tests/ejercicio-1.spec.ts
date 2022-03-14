730dbb7c2d5cd18e5fcf844b7e67e31be579df40
Andrea Calero Caro <alu0101202952@ull.edu.es>
2022-03-14 10:15:07 +0000

    Test getFNacimientoE() TDD estudiante

--- pe102/tests/ejercicio-1.spec.ts
+++ pe102/tests/ejercicio-1.spec.ts
@@ -33,13 +33,13 @@ describe('Estudiante', () => {
   it('estudiante.getApellidosE() returns apellido del estudiante', () => {
     return expect(estudiante.getApellidosE()).to.be.equal('Calero');
   })
-/*
-  it('persona.getFNacimiento() returns apellido de la persona', () => {
-    return expect(persona.getFNacimiento()).to.be.equal('08-04-2000');
-  })
 
+  it('estudiante.getFNacimientoE() returns apellido del estudiante', () => {
+    return expect(estudiante.getFNacimientoE()).to.be.equal('08-04-2000');
+  })
+/*
   it('persona.getGenero() returns apellido de la persona', () => {
     return expect(persona.getGenero()).to.be.equal('Mujer');
-  })*/
-
+  })
+*/
 });
