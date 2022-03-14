3d6d72235cc935d7935d4cb55e1550397c48cc32
Andrea Calero Caro <alu0101202952@ull.edu.es>
2022-03-14 09:48:50 +0000

    Código getNombre()

--- pe102/src/ejercicio-1.ts
+++ pe102/src/ejercicio-1.ts
@@ -1,11 +1,34 @@
-interface Persona {
+export interface Persona {
     nombre: string;
     apellidos: string;
     fecha_de_nacimiento: string;
-    género: string;
+    genero: string;
     /*sides?: number;
     color: string;
     getArea(): number;
     print(): void;*/
+
+}
+
+/**
+ * PE-102 Clase Alumno implementa interfaz persona
+ * @class Alumno tiene atributos de los alumnos
+ * @param nombre del alumno
+ * @param apellidos del alumno
+ * @param fecha_de_nacimiento del alumno
+ * @param genero del alumno
+ */
+export class Alumno implements Persona {
+    constructor(private nombre: string, private apellidos: string,
+      private fecha_de_nacimiento: string, private genero: string) {
+    }
+
+    setNombre(nombre: string) {
+        this.nombre = nombre;
+    }
+
+    getNombre(){
+        return this.nombre;
+    }
 
 }
