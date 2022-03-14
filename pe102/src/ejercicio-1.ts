42ee2fa6409039eada0d4ba95cb26b91f5478da2
Andrea Calero Caro <alu0101202952@ull.edu.es>
2022-03-14 09:39:54 +0000

    Test getNombre() TDD

--- pe102/src/ejercicio-1.ts
+++ pe102/src/ejercicio-1.ts
@@ -1,26 +1,11 @@
-/**
- * EJERCICIO 1: Devolver un array de arrays con n tablas de multiplicas donde cada tabla de multiplicar contiene los primeros n productos de valores
- * @param n numero de parametros de las tablas
- * @returns array de arrays con n tablas de multiplicar
- */
-export function productTable(n: number){
-    let resultado: number [] = [];
-    let tabla: number [][] = [];
-    for (let i: number = 1; i <= n; i++) {
-        for (let j: number = 1; j <= n; j++) {
-            resultado.push(i*j);
-        }
-        tabla.push(resultado);
-        resultado = [];
-    }
-    console.log(tabla);
-    return tabla;
-    
-}
+interface Persona {
+    nombre: string;
+    apellidos: string;
+    fecha_de_nacimiento: string;
+    género: string;
+    /*sides?: number;
+    color: string;
+    getArea(): number;
+    print(): void;*/
 
-let n: number = 3;
-let n1: number = 4;
-console.log('\nTabla de n = '+ n+' es: ');
-productTable(n);
-console.log('\nTabla de n = '+ n1+' es: ');
-productTable(n1);
+}
--- pe102/tests/ejercicio-1.spec.ts
+++ pe102/tests/ejercicio-1.spec.ts
@@ -1,16 +1,12 @@
 import 'mocha';
 import {expect} from 'chai';
-import {productTable} from '../src/ejercicio-1';
+import {Persona} from '../src/ejercicio-1';
 
-let array1 = [[1, 2, 3], [2, 4, 6], [3, 6, 9]];
-let array2 = [[1, 2, 3, 4], [2, 4, 6, 8], [3, 6, 9, 12], [4, 8, 12, 16]];
+let persona = new Persona('Andrea', 'Calero', '08-04-2000', 'Mujer');
 
-describe('Devuelve un arra de arrays con n (valor que se pasa) tablas de multiplicar', () => {
-    it('productTable(3) retorna el valor array1', () => {
-      expect(productTable(3)).to.be.eql(array1);
-    });
+describe('Persona', () => {
+    it('persona.getNombre() returns nombre de la persona', () => {
+        return expect(persona.getNombre()).to.be.equal('Andrea');
+    })
   
-    it('productTable(4) retorna el valor array2', () => {
-      expect(productTable(4)).to.be.eql(array2);
-    });
 });
