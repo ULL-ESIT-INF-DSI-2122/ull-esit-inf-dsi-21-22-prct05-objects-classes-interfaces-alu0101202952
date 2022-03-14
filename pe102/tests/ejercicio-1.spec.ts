ca276e110e99a0373208a926a345ea8a9d8d89c2
Andrea Calero Caro <alu0101202952@ull.edu.es>
2022-03-14 10:33:36 +0000

    Test getNombreAsig() TDD asignatura

--- pe102/tests/ejercicio-1.spec.ts
+++ pe102/tests/ejercicio-1.spec.ts
@@ -1,10 +1,11 @@
 import 'mocha';
 import {expect} from 'chai';
-import {Persona, Estudiante, Profesor} from '../src/ejercicio-1';
+import {Persona, Estudiante, Profesor, Asignatura} from '../src/ejercicio-1';
 
 let persona = new Persona('Andrea', 'Calero', '08-04-2000', 'Mujer');
 let estudiante = new Estudiante('Andrea', 'Calero', '08-04-2000', 'Mujer', 'alu0101202952@ull.edu.es');
 let profesor = new Profesor('Juan', 'Glez', '09-04-2000', 'Hombre', 'juan@ull.es');
+let asignatura = new Asignatura ('DSI', ['Andrea'], ['Juan']);
 
 describe('Persona', () => {
     it('persona.getNombre() returns nombre de la persona', () => {
@@ -35,5 +36,11 @@ describe('Estudiante', () => {
 describe('Profesor', () => {
   it('profesor.getEmailP() returns email del profesor', () => {
       return expect(profesor.getEmailP()).to.be.equal('juan@ull.es');
+  })
+});
+
+describe('Asignatura', () => {
+  it('asignatura.getNombreAsig() returns nombre de la asignatura', () => {
+      return expect(asignatura.getNombreAsig()).to.be.equal('DSI');
   })
 });
