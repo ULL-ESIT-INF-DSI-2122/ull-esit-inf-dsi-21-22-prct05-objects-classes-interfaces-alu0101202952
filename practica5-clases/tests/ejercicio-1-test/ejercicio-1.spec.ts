c9ed24318454f44333919ea22ac27a72dcbb7435
Andrea Calero Caro <alu0101202952@ull.edu.es>
2022-03-17 20:25:11 +0000

    Test class ejercicio 1, Pokedex getPokemons() TDD

--- practica5-clases/tests/ejercicio-1-test/ejercicio-1.spec.ts
+++ practica5-clases/tests/ejercicio-1-test/ejercicio-1.spec.ts
@@ -1,28 +1,37 @@
 import 'mocha';
 import {expect} from 'chai';
 import {Pokemon} from '../../src/ejercicio-1/pokemon';
+import {Pokedex} from '../../src/ejercicio-1/pokedex';
 
 let pokemon1 = new Pokemon('Flareon', 25, 0.9, 'Fuego', [130, 60, 65, 65]);
+let pokedex = new Pokedex([pokemon1]);
 
 describe('Pokemon', () => {
-    it('pokemon1.getNombre() returns nombre del pokemon', () => {
-        return expect(pokemon1.getNombre()).to.be.equal('Flareon');
-    })
+  it('pokemon1.getNombre() returns nombre del pokemon', () => {
+      return expect(pokemon1.getNombre()).to.be.equal('Flareon');
+  })
 
-    it('pokemon1.getPeso() returns peso del pokemon', () => {
-      return expect(pokemon1.getPeso()).to.be.equal(25);
-    })
+  it('pokemon1.getPeso() returns peso del pokemon', () => {
+    return expect(pokemon1.getPeso()).to.be.equal(25);
+  })
 
-    it('pokemon1.getAltura() returns altura del pokemon', () => {
-      return expect(pokemon1.getAltura()).to.be.equal(0.9);
-    })
+  it('pokemon1.getAltura() returns altura del pokemon', () => {
+    return expect(pokemon1.getAltura()).to.be.equal(0.9);
+  })
 
-    it('pokemon1.getTipo() returns tipo del pokemon', () => {
-      return expect(pokemon1.getTipo()).to.be.equal('Fuego');
-    })
+  it('pokemon1.getTipo() returns tipo del pokemon', () => {
+    return expect(pokemon1.getTipo()).to.be.equal('Fuego');
+  })
 
-    it('pokemon1.getEstadisticas() returns estadísticas del pokemon', () => {
-      return expect(pokemon1.getEstadisticas()).to.be.equal([130, 60, 65, 65]);
-    })
+  it('pokemon1.getEstadisticas() returns estadísticas del pokemon', () => {
+    return expect(pokemon1.getEstadisticas()).to.be.eql([130, 60, 65, 65]);
+  })
   
+});
+
+describe('Pokedex', () => {
+  it('pokedex.getPokemons() returns pokemons de la pokedex', () => {
+    return expect(pokedex.getPokemons()).to.be.eql([pokemon1]);
+  })
+
 });
