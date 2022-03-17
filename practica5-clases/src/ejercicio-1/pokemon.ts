6cefe39b3cf96dea82f9a2c93c6178bfe377861e
Andrea Calero Caro <alu0101202952@ull.edu.es>
2022-03-17 18:42:21 +0000

    Código class ejercicio 1, Pokemon getPeso() TDD

--- practica5-clases/src/ejercicio-1/pokemon.ts
+++ practica5-clases/src/ejercicio-1/pokemon.ts
@@ -1,4 +1,12 @@
-
+/**
+ * EJERCICIO 1.
+ * @class Pokemon, tiene las especificaciones correspondientes a los pokemon
+ * @param nombre tiene el nombre del pokemon (string)
+ * @param peso tiene el peso del pokemon, Kg (number)
+ * @param altura tiene la altura del pokemon, m (number)
+ * @param tipo tiene el tipo de pokemon (string)
+ * @param estadisticas tiene un array del ataque, defensa, velocidad y daño máximo del pokemon (number [])
+ */
 export class Pokemon {
     protected estadisticas = {
         ataque: 0,
@@ -15,13 +23,36 @@ export class Pokemon {
         this.estadisticas.hp = estadisticas[3];
     }
     
+    /**
+     * Setter nombre del pokemon
+     * @param nombre del pokemon
+     */
     setNombre(nombre: string) {
         this.nombre = nombre;
     }
 
-
+    /**
+     * Getter del nombre del pokemon
+     * @returns nombre del pokemon
+     */
     getNombre(){
         return this.nombre;
+    }
+
+    /**
+     * Setter peso del pokemon
+     * @param peso del pokemon
+     */
+     setPeso(peso: number) {
+        this.peso = peso;
+    }
+
+    /**
+     * Getter del peso del pokemon
+     * @returns peso del pokemon
+     */
+    getPeso(){
+        return this.peso;
     }
 
 }
@@ -31,7 +62,11 @@ export class Pokemon {
  * Salidas por pantalla
  */
 let pokemon1 = new Pokemon('Flareon', 25, 0.9, 'Fuego', [130, 60, 65, 65]);
+console.log(pokemon1);
 pokemon1.setNombre('Flareon (Eeveevolución)');
 console.log(pokemon1);
-
 pokemon1.getNombre();
+
+pokemon1.setPeso(27);
+console.log(pokemon1);
+pokemon1.getPeso();
