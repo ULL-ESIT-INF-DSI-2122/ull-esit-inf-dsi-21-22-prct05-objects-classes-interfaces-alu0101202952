4b28e4315f88c66b71decf36839a9ed90bf089af
Andrea Calero Caro <alu0101202952@ull.edu.es>
2022-03-17 18:22:30 +0000

    Test class ejercicio 1, Pokemon getNombre() TDD

--- practica5-clases/tests/ejercicio-1-test/ejercicio-1.spec.ts
+++ practica5-clases/tests/ejercicio-1-test/ejercicio-1.spec.ts
@@ -1,17 +1,17 @@
 import 'mocha';
 import {expect} from 'chai';
-import {Persona, Estudiante, Profesor, Asignatura} from '../src/ejercicio-1';
+import {Pokemon} from '../../src/ejercicio-1/pokemon';
 
-let persona = new Persona('Andrea', 'Calero', '08-04-2000', 'Mujer');
-let estudiante = new Estudiante('Andrea', 'Calero', '08-04-2000', 'Mujer', 'alu0101202952@ull.edu.es');
-let profesor = new Profesor('Juan', 'Glez', '09-04-2000', 'Hombre', 'juan@ull.es');
-let asignatura = new Asignatura ('DSI', [estudiante], [profesor]);
+let pokemon1 = new Pokemon('Flareon', 25, 0.9, 'Fuego', [130, 60, 65, 65]);
+//let estudiante = new Estudiante('Andrea', 'Calero', '08-04-2000', 'Mujer', 'alu0101202952@ull.edu.es');
+//let profesor = new Profesor('Juan', 'Glez', '09-04-2000', 'Hombre', 'juan@ull.es');
+//let asignatura = new Asignatura ('DSI', [estudiante], [profesor]);
 
-describe('Persona', () => {
-    it('persona.getNombre() returns nombre de la persona', () => {
-        return expect(persona.getNombre()).to.be.equal('Andrea');
+describe('Pokemon', () => {
+    it('pokemon1.getNombre() returns nombre del pokemon', () => {
+        return expect(pokemon1.getNombre()).to.be.equal('Flareon');
     })
-
+/*
     it('persona.getApellidos() returns apellido de la persona', () => {
       return expect(persona.getApellidos()).to.be.equal('Calero');
     })
@@ -22,11 +22,11 @@ describe('Persona', () => {
 
     it('persona.getGenero() returns apellido de la persona', () => {
       return expect(persona.getGenero()).to.be.equal('Mujer');
-    })
+    })*/
   
 });
 
-
+/*
 describe('Estudiante', () => {
   it('estudiante.getEmail() returns email del alumno', () => {
       return expect(estudiante.getEmail()).to.be.equal('alu0101202952@ull.edu.es');
@@ -50,4 +50,4 @@ describe('Asignatura', () => {
   it('asignatura.getNombreProfesor() returns nombre de los profesores de la asignatura', () => {
     return expect(asignatura.getNombreProfesor()).to.be.eql([profesor]);
   })
-});
+});*/
