9d526aa5e39323692751b93d26963a815a7d6ad6
Andrea Calero Caro <alu0101202952@ull.edu.es>
2022-03-20 10:59:08 +0000

    Documentación ejericio 2 Tablero generarTablero() TDD

--- practica5-clases/src/ejercicio-2/tablero.ts
+++ practica5-clases/src/ejercicio-2/tablero.ts
@@ -1,7 +1,15 @@
-
+/**
+ * EJERCICIO 2. Conecta 4
+ * @class Tablero, tiene la generación del tablero a trabajar
+ * @param filas del tablero (6)
+ * @param columnas del tablero (7)
+ */
 export class Tablero{
     constructor (private filas: number, private columnas: number){}
 
+    /**
+     * Método público que genera el tablero llenando el array con el espacio reservado de filas y columnas
+     */
     public generarTablero(){
         let tablero: number [] = [];
         for (let i: number = 0; i < this.filas; i++) {
@@ -12,6 +20,9 @@ export class Tablero{
     }
 }
 
+/**
+ * Salida por pantalla
+ */
 let tablero = new Tablero(6,7);
 tablero.generarTablero();
 console.log(tablero);
