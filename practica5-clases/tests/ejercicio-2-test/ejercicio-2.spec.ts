487d020b2a123aae6ecd49cd833719dd4bbe6a53
Andrea Calero Caro <alu0101202952@ull.edu.es>
2022-03-20 10:56:09 +0000

    Coverage ejericio 2 Tablero generarTablero() TDD

--- practica5-clases/tests/ejercicio-2-test/ejercicio-2.spec.ts
+++ practica5-clases/tests/ejercicio-2-test/ejercicio-2.spec.ts
@@ -2,9 +2,7 @@ import 'mocha';
 import {expect} from 'chai';
 import {Tablero} from '../../src/ejercicio-2/tablero';
 
-
 let tablero = new Tablero(6,7);
-
 
 describe('Tablero Conecta 4', () => {
   it('tablero.generarTablero() returns tablero 6*7', () => {
