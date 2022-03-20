9bcd99c326d1cddb51a37aa4fd34a34e8e398bd2
Andrea Calero Caro <alu0101202952@ull.edu.es>
2022-03-20 11:02:26 +0000

    Test ejericio 2 Juego movimiento() TDD

--- practica5-clases/tests/ejercicio-2-test/ejercicio-2.spec.ts
+++ practica5-clases/tests/ejercicio-2-test/ejercicio-2.spec.ts
@@ -1,12 +1,21 @@
 import 'mocha';
 import {expect} from 'chai';
 import {Tablero} from '../../src/ejercicio-2/tablero';
+import {Juego} from '../../src/ejercicio-2/juego';
 
 let tablero = new Tablero(6,7);
+let juego = new Juego();
 
 describe('Tablero Conecta 4', () => {
   it('tablero.generarTablero() returns tablero 6*7', () => {
       return expect(tablero.generarTablero()).to.be.eql('Flareon');
+  })
+
+});
+
+describe('Juego Conecta 4', () => {
+  it('juego.movimiento() returns se valida si la columna está llena o no', () => {
+      return expect(juego.movimiento()).to.be.true;
   })
 
 });
